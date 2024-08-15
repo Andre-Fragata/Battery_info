51575a3bda4300bbb25a66ba5f88410f26265df5
Andre Fragata <andrefragata14@gmail.com>
2024-08-15 13:32:49 -0300

    atualizacao

---
+++ battery_info/target/debug/deps/battery_info-cf080821715c3610.d
@@ -0,0 +1,5 @@
+/home/owlin/repos/Battery_info/battery_info/target/debug/deps/battery_info-cf080821715c3610: src/main.rs
+
+/home/owlin/repos/Battery_info/battery_info/target/debug/deps/battery_info-cf080821715c3610.d: src/main.rs
+
+src/main.rs:
